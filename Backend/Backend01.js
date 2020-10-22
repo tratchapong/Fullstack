fbb4cd8196b8839d563efea0c23b598913b92cd7
tratchapong <tratchapong@gmail.com>
2020-10-22 18:08:02 +0700

    First init

---
+++ Backend/Backend01.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const fs = require('fs')
+
+app.use(cors())
+app.use(express.json())
+// app.use(express.urlencoded( {extended:false} ))
+
+let data = JSON.parse(fs.readFileSync('./songs.json'))
+let {songs} = data
+console.log(data)
+console.log(songs)
+
+app.get('/songs', (req, res) => {
+  res.status(200).send(data.songs)
+})
+
+app.get('/songs/:id', (req, res) => {
+  output = songs.find( x=> x.id === +req.params.id)
+  res.status(200).send(output)
+})
+
+app.get('/songname/:name', (req, res) => {
+  output = songs.find( x=> x.name === req.params.name)
+  res.status(200).send(output)
+})
+
+app.get('/songsReset', (req, res) => {
+  console.log('-------')
+  console.log({songs})
+  songs = []
+  console.log({songs})
+  console.log('-------')
+  let wdata = { songs }
+  res.status(200).send('Reset all data')
+  fs.writeFileSync('./songs.json',JSON.stringify(wdata))
+})
+
+app.post('/songs/:name', (req, res) => {
+  let new_id = songs.length>0 ? songs[songs.length-1].id + 1 : 1
+  let new_song = {id: new_id, name: req.params.name}
+  songs.push(new_song)
+  res.status(201).send(new_song)
+  let wdata = { songs }
+  // console.log(wdata)
+  fs.writeFileSync('./songs.json',JSON.stringify(wdata))
+})
+
+app.post('/songadd', (req, res) => {
+  console.log('inside-songsadd ')
+})
+
+app.listen(5000, ()=> console.log("Server start..."))
